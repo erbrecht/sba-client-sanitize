a09814350433fb2c1ba91537eb7e9b20341bbaa1
Travis Collins <tec25@drexel.edu>
2023-07-21 10:43:17 -0400

    initial commit

---
+++ src/main/java/com/example/sbadminclient/SbAdminClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.sbadminclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbAdminClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbAdminClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sbadminclient/SbAdminClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sbadminclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbAdminClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
